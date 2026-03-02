39fba7f15e48d800aa791d9c6c59ef14edd4710d
Sveske-Juice <sveske_juice@tuta.com>
2026-03-02 08:01:27 +0100

    refactor(ip): move things to let bindings

--- modules/ip.nix
+++ modules/ip.nix
@@ -1,72 +1,74 @@
-{lib, ...}: {
-  options = {
-    ip = lib.mkOption {
+{lib, ...}: let
+  httpOpts = lib.types.submodule (_: {
+    options.secureServer = lib.mkOption {
+      description = "HTTPS configuration server";
       default = {};
       type = lib.types.submodule (_: {
         options = {
-          routing = lib.mkOption {
+          enable = lib.mkOption {
             type = lib.types.bool;
-            default = false;
+            default = true;
           };
-          domainName = lib.mkOption {
-            type = lib.types.nullOr lib.types.str;
-            default = null;
-          };
-          defaultGateway = lib.mkOption {
-            type = lib.types.nullOr lib.types.str;
-            description = "ipv4 host address of DGW";
-            default = null;
-          };
-          nameServers = lib.mkOption {
-            type = lib.types.listOf lib.types.str;
-            default = [];
-            description = "List of ipv4 and ipv6 name servers to use. Max 6 for each ip version";
-          };
-          domainLookup = lib.mkOption {
+          options.server = lib.mkOption {
+            description = "HTTP configuration server";
+            default = {};
             type = lib.types.submodule (_: {
               options = {
                 enable = lib.mkOption {
                   type = lib.types.bool;
-                  default = false;
+                  default = true;
                 };
-                # TODO: nsap, recursive, source-interface, vrf
-              };
-            });
-            default = {};
-          };
-          http = lib.mkOption {
-            default = {};
-            type = lib.types.submodule (_: {
-              options.secureServer = lib.mkOption {
-                description = "HTTPS configuration server";
-                default = {};
-                type = lib.types.submodule (_: {
-                  options = {
-                    enable = lib.mkOption {
-                      type = lib.types.bool;
-                      default = true;
-                    };
-                    # TODO: other settings
-                  };
-                });
-              };
-              options.server = lib.mkOption {
-                description = "HTTP configuration server";
-                default = {};
-                type = lib.types.submodule (_: {
-                  options = {
-                    enable = lib.mkOption {
-                      type = lib.types.bool;
-                      default = true;
-                    };
-                    # TODO: other settings
-                  };
-                });
+                # TODO: other settings
               };
             });
           };
+          # TODO: other settings
         };
       });
+    };
+  });
+  ipOpts = lib.types.submodule (_: {
+    options = {
+      routing = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+      };
+      domainName = lib.mkOption {
+        type = lib.types.nullOr lib.types.str;
+        default = null;
+      };
+      defaultGateway = lib.mkOption {
+        type = lib.types.nullOr lib.types.str;
+        description = "ipv4 host address of DGW";
+        default = null;
+      };
+      nameServers = lib.mkOption {
+        type = lib.types.listOf lib.types.str;
+        default = [];
+        description = "List of ipv4 and ipv6 name servers to use. Max 6 for each ip version";
+      };
+      domainLookup = lib.mkOption {
+        type = lib.types.submodule (_: {
+          options = {
+            enable = lib.mkOption {
+              type = lib.types.bool;
+              default = false;
+            };
+            # TODO: nsap, recursive, source-interface, vrf
+          };
+        });
+        default = {};
+      };
+      http = lib.mkOption {
+        type = httpOpts;
+        default = {};
+      };
+    };
+  });
+in {
+  options = {
+    ip = lib.mkOption {
+      type = ipOpts;
       default = {};
     };
   };
