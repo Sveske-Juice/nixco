f649be1dcb9ba6574d892be72fdcb25829e9d2a1
Sveske-Juice <sveske_juice@tuta.com>
2026-04-16 22:22:04 +0200

    feat: ipv6

---
+++ nix/modules/ipv6.nix
@@ -0,0 +1,19 @@
+{inputs, ...}: let
+  inherit (inputs.nixpkgs) lib;
+in {
+  flake.nixcoModules.ipv6 = {
+    options = {
+      ipv6 = lib.mkOption {
+        description = "Global IPv6 Configuration";
+        type = lib.types.submodule {
+          options = {
+            routing = lib.mkOption {
+              type = lib.types.bool;
+              default = false;
+            }; 
+          };
+        };
+      };
+    };
+  };
+}
