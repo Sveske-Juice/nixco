825361af082080a558be42469e6720a1255a2f5d
Sveske-Juice <sveske_juice@tuta.com>
2026-04-16 22:21:13 +0200

    feat: ipv4 custom type

--- nix/lib/types.nix
+++ nix/lib/types.nix
@@ -2,9 +2,15 @@
   inherit (inputs.nixpkgs) lib;
 in {
   flake.lib.types = {
-    ipv4Address = lib.mkOptionType {
-      name = "ipAddress";
+    ipv4 = lib.mkOptionType {
+      name = "ipv4Address";
       description = "IPv4 Address";
+      check = str: builtins.match "[0-9]{1,3}(\.[0-9]{1,3}){3}" str != null;
+    };
+    # FIXME: update this
+    ipv6 = lib.mkOptionType {
+      name = "ipv6Address";
+      description = "IPv6 Address";
       check = str: builtins.match "[0-9]{1,3}(\.[0-9]{1,3}){3}" str != null;
     };
   };
