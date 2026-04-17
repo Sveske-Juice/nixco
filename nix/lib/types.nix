12c8b42823c20596c23a7f9d95a667fe9f62844d
Sveske-Juice <sveske_juice@tuta.com>
2026-04-17 09:18:55 +0200

    feat: ipv4net type

--- nix/lib/types.nix
+++ nix/lib/types.nix
@@ -1,17 +1,32 @@
 {inputs, ...}: let
   inherit (inputs.nixpkgs) lib;
+  isValidipv4 = str: builtins.match "[0-9]{1,3}(\.[0-9]{1,3}){3}" str != null;
+  # TODO: 
+  isValidNetmask = mask: true;
 in {
   flake.lib.types = {
     ipv4 = lib.mkOptionType {
       name = "ipv4Address";
       description = "IPv4 Address";
-      check = str: builtins.match "[0-9]{1,3}(\.[0-9]{1,3}){3}" str != null;
+      check = isValidipv4;
     };
-    # FIXME: update this
+
+    ipv4Network = lib.mkOptionType {
+      name = "ipv4Network";
+      description = "IPV4 Network";
+      check = v:
+        lib.isAttrs v
+        && v ? addr
+        && v ? netmask
+        && isValidipv4 v.addr
+        && isValidNetmask v.netmask;
+    };
+
     ipv6 = lib.mkOptionType {
       name = "ipv6Address";
       description = "IPv6 Address";
-      check = str: builtins.match "[0-9]{1,3}(\.[0-9]{1,3}){3}" str != null;
+      # FIXME: update this
+      check = isValidipv4;
     };
   };
 }
