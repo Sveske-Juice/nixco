389e4f8b4184732df60320260d20e42d119b64d8
Sveske-Juice <sveske_juice@tuta.com>
2026-04-17 14:14:55 +0200

    feat: builtin device specs

--- nix/flake-parts/export-flake-module.nix
+++ nix/flake-parts/export-flake-module.nix
@@ -1,8 +1,73 @@
-{self, ...}: let
-  nixcoModule = import ../../flake-module.nix {inherit self;};
-in {
-  flake.flakeModules.nixco = nixcoModule;
+{self, inputs, ...}: {
   flake.flakeModules.default = self.flakeModules.nixco;
+  flake.flakeModules.nixco = {config, lib, ...}: {
+    options.nixco = lib.mkOption {
+      type = lib.types.submodule {
+        options = {
+          devices = lib.mkOption {
+            type = lib.types.attrsOf (lib.types.submodule {
+              imports = [
+                self.nixcoModules.misc
+                self.nixcoModules.banner
+                self.nixcoModules.device
+                self.nixcoModules.vlans
+                self.nixcoModules.ip
+                self.nixcoModules.ipv6
+                self.nixcoModules.interfaces
+                self.nixcoModules.eem
+              ];
+            });
+            default = {};
+          };
+          flakeCheck = lib.mkOption {
+            type = lib.types.bool;
+            default = true;
+            example = false;
+            description = ''
+            Automatically add all nixcoConfigurations to.`checks.<system>`.
+            '';
+          };
+          deviceSpecs = lib.mkOption {
+            default = {};
+            description = "This device's hardware specification";
+            type = lib.types.attrsOf (lib.types.submodule {
+              options = {
+                name = lib.mkOption {
+                  type = lib.types.str;
+                  description = "Name of device";
+                };
+                interfaces = lib.mkOption {
+                  type = lib.types.listOf lib.types.str;
+                  default = [];
+                  description = "The interfaces this device have";
+                };
+              };
+            });
+          };
+        };
+      };
+      default = {};
+    };
 
-  imports = [nixcoModule];
+    config = {
+      flake.nixcoConfigurations = self.lib.renderAll config.nixco.devices;
+      # Builtin device specs
+      nixco.deviceSpecs = lib.foldl' lib.mergeAttrs {}
+        (lib.mapAttrsToList
+          (fname: _: import (../lib/_devices + "/${fname}"))
+          (builtins.readDir ../lib/_devices)
+        );
+      perSystem = {pkgs, ...}: let
+        devicePackages =
+          lib.mapAttrs (
+            name: text:
+            pkgs.writeText "${name}.ios" text
+          )
+          config.flake.nixcoConfigurations;
+      in {
+        packages = devicePackages;
+        checks = lib.mkIf config.nixco.flakeCheck devicePackages;
+      };
+    };
+  };
 }
