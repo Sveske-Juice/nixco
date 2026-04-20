dd57b04d62385d9b50501b983be0dec50dd82bd0
Sveske-Juice <sveske_juice@tuta.com>
2026-04-20 16:37:01 +0200

    feat: ISR4331

---
+++ nix/lib/_devices/ISR4331.nix
@@ -0,0 +1,11 @@
+{
+  "ISR4331" = {
+    name = "ISR4331";
+    deviceType = "router";
+    interfaces = [
+      "GigabitEthernet0/0/0"
+      "GigabitEthernet0/0/1"
+      "GigabitEthernet0/0/2"
+    ];
+  };
+}
