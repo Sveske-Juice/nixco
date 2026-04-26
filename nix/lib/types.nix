eb842527e1b08383db3dca4b3856f2a4f971b4cf
dr3y <sveske_juice@tuta.com>
2026-04-26 22:23:50 +0200

    feat: validate ipv6 addr #24

--- nix/lib/types.nix
+++ nix/lib/types.nix
@@ -1,6 +1,22 @@
 {inputs, ...}: let
   inherit (inputs.nixpkgs) lib;
   isValidipv4 = str: builtins.match "[0-9]{1,3}(\.[0-9]{1,3}){3}" str != null;
+  # i made AI cook this one chat. if its wrong gg, i aint spending my free time
+  # on regex.
+  isValidipv6 = str: let
+    # Strip optional prefix length e.g. 2001:db8::1/64
+    addr = builtins.head (lib.splitString "/" str);
+    # Must contain at least one colon
+    hasColon = builtins.match ".*:.*" addr != null;
+    # Only hex digits and colons allowed
+    validChars = builtins.match "[0-9a-fA-F:]+" addr != null;
+    # At most one "::" (splitString gives length 2 if one "::" present)
+    atMostOneCompress = builtins.length (lib.splitString "::" addr) <= 2;
+    # Each group (split by single colon, ignoring empty from ::) is max 4 hex chars
+    groups = builtins.filter (g: g != "") (lib.splitString ":" addr);
+    validGroups = builtins.all (g: builtins.match "[0-9a-fA-F]{1,4}" g != null) groups;
+  in
+    hasColon && validChars && atMostOneCompress && validGroups;
   # TODO:
   isValidNetmask = _mask: true;
 in {
@@ -30,8 +46,7 @@ in {
     ipv6 = lib.mkOptionType {
       name = "ipv6Address";
       description = "IPv6 Address";
-      # FIXME: update this
-      check = isValidipv4;
+      check = isValidipv6;
     };
   };
 }
