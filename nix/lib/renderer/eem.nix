52f4d79b4cddd5c318e16bd71fdd6e2e1180bc5a
dr3y <sveske_juice@tuta.com>
2026-04-26 20:49:52 +0200

    feat(render): indent eem

--- nix/lib/renderer/eem.nix
+++ nix/lib/renderer/eem.nix
@@ -1,29 +1,32 @@
-{inputs, ...}: let
+{inputs, self, ...}: let
   inherit (inputs.nixpkgs) lib;
-  renderEEMApplet = label: applet:
-    ''
-      event manager applet ${label}
-    ''
-    + lib.optionalString (applet.description != null) "description ${applet.description}\n"
-    +
-    # Render event
-    ''
+in {
+  flake.lib.renderEEMApplet = label: applet: let
+    body = 
+      lib.optionalString (applet.description != null) "description ${applet.description}\n"
+      +
+      # Render event
+      ''
       event ${applet.event.eventStr}
-    ''
-    +
-    # Render actions
-    (builtins.concatStringsSep "\n" (map (
+      ''
+      +
+      # Render actions
+      (builtins.concatStringsSep "\n" (map (
         action: ''action ${action.label} ${action.actionStr}''
       )
-      applet.actions))
-    + "\n";
-in {
+        applet.actions))
+      + "\n";
+  in 
+    "event manger applet ${label}"
+    + "\n"
+    + self.lib.indentLines body;
+
   flake.lib.renderEEM = eem:
   # Render all applets
     builtins.concatStringsSep "\n"
     (
       lib.attrsets.mapAttrsToList
-      (label: applet: renderEEMApplet label applet)
+      (label: applet: self.lib.renderEEMApplet label applet)
       eem.applets
     );
 }
